95d80a2d626e567a0eeb2fedc30725c4c9b18c34
Lorenz <lorenzgillesania20@gmail.com>
2023-05-03 13:40:51 +0800

    added 'getUser' and 'getUsers' functionalities

--- controllers/users/getUser.js
+++ controllers/users/getUser.js
@@ -1,4 +1,20 @@
-const getUser = (req, res) => {
-    res.send('getUser');
+const User = require('./../../models/User');
+const { validateId } = require('./../../functions/validation');
+const getUser = async (req, res) => {
+    const id = req.params.id;
+    const validatedId = validateId(id);
+    let findUser = null;
+    if(
+        validatedId === false
+    ){
+        return res.json({message: 'invalid id'});
+    }
+    findUser = await User.findOne({_id: id}, 'username role active');
+    if(
+        findUser === null
+    ){
+        return res.json({message: 'user not found'});
+    }
+    return res.json(findUser);
 }
 module.exports = getUser;
--- controllers/users/getUsers.js
+++ controllers/users/getUsers.js
@@ -1,5 +1,13 @@
-const getUsers = (req, res) => {
-    res.send('getUsers');
+const User = require('./../../models/User');
+const getUsers = async (req, res) => {
+    let findUsers = [];
+    findUsers = await User.find({}, 'username role active');
+    if(
+        findUsers === []
+    ){
+        res.json({message: 'no users yet'});
+    }
+    return res.json(findUsers)
 }
 
 module.exports = getUsers;
