0b668d8927cfded762fa7bdf97ea8ac4ab687b46
Lorenz <lorenzgillesania20@gmail.com>
2023-05-16 11:43:58 +0800

    added unit testing with 'logout.test.js'

---
+++ __tests__/logout.test.js
@@ -0,0 +1,16 @@
+const logout = require('./../controllers/auth/logout');
+describe('POST api/auth/logout', () => {
+    test('logout success', async () => {
+        const STATIC_NAME = 'jwt';
+        const req = jest.fn();
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+            clearCookie: jest.fn()
+        }
+        await logout(req, res);
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({message: 'logout successful'});
+        expect(res.clearCookie).toHaveBeenCalledWith(STATIC_NAME);
+    });
+});
