6d18125ad82c49b1554fe253ba20b15b76636af4
Lorenz <lorenzgillesania20@gmail.com>
2023-05-17 22:03:52 +0800

    added unit testing with 'getUsers.test.js'

---
+++ __tests__/controllers/users/getUsers.test.js
@@ -0,0 +1,52 @@
+const getUsers = require('./../../../controllers/users/getUsers');
+const { userFind } = require('./../../../models/user/userQueries');
+jest.mock('./../../../models/user/userQueries');
+describe('GET api/users', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+    test('no users', async () => {
+        const STATIC_PROJECTION = 'username role active'
+        const req = {};
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+        userFind.mockResolvedValue([]);
+        await getUsers(req, res);
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({
+            message: 'no users yet',
+            users: []
+        });
+        expect(userFind).toHaveBeenCalledWith({}, STATIC_PROJECTION);
+    });
+    test('successful get users', async () => {
+        const STATIC_PROJECTION = 'username role active';
+        const req = {};
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+        userFind.mockResolvedValue([
+            {
+                username: 'user1',
+                role: 'role',
+                active: 'active'
+            }
+        ]);
+        await getUsers(req, res);
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({
+            message: 'users found',
+            users: [
+                {
+                    username: 'user1',
+                    role: 'role',
+                    active: 'active'
+                }
+            ]
+        });
+        expect(userFind).toHaveBeenCalledWith({}, STATIC_PROJECTION);
+    });
+});
