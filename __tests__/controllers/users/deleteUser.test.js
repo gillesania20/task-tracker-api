d1aad73f9d02425cc4182ca4746d5be5bf8ecf01
Lorenz <lorenzgillesania20@gmail.com>
2023-05-17 21:12:21 +0800

    added unit testing with 'deleteUser.test.js'

---
+++ __tests__/controllers/users/deleteUser.test.js
@@ -0,0 +1,73 @@
+const deleteUser = require('./../../../controllers/users/deleteUser');
+const { userFindOne, userDeleteOne } = require('./../../../models/user/userQueries');
+const { validateId } = require('./../../../functions/validation');
+jest.mock('./../../../models/user/userQueries');
+jest.mock('./../../../functions/validation');
+describe('DELETE api/users/:userId', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+    test('invalid id', async () => {
+        const req = {
+            params: {
+                id: 'id'
+            }
+        };
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+        validateId.mockReturnValue(false);
+        await deleteUser(req, res);
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({
+            message: 'invalid id'
+        });
+        expect(validateId).toHaveBeenCalledWith('id');
+    });
+    test('user not found', async () => {
+        const STATIC_PROJECTION = '_id';
+        const req = {
+            params: {
+                id: 'id'
+            }
+        };
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+        validateId.mockReturnValue(true);
+        userFindOne.mockResolvedValue(null);
+        await deleteUser(req, res);
+        expect(res.status).toHaveBeenCalledWith(404);
+        expect(res.json).toHaveBeenCalledWith({
+            message: 'user not found'
+        });
+        expect(validateId).toHaveBeenCalledWith('id');
+        expect(userFindOne).toHaveBeenCalledWith({
+            _id: 'id'
+        }, STATIC_PROJECTION);
+    });
+    test('successful delete user', async () => {
+        const STATIC_PROJECTION = '_id';
+        const req = {
+            params: {
+                id: 'id'
+            }
+        };
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+        validateId.mockReturnValue(true);
+        userFindOne.mockResolvedValue({_id: '_id'});
+        await deleteUser(req, res);
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({message: 'user deleted'});
+        expect(validateId).toHaveBeenCalledWith('id');
+        expect(userFindOne).toHaveBeenCalledWith({
+            _id: 'id'
+        }, STATIC_PROJECTION);
+        expect(userDeleteOne).toHaveBeenCalledWith({ _id: 'id'});
+    });
+});
