f60ef02a340a38d622068ea2c634161737cf9a4d
Lorenz <lorenzgillesania20@gmail.com>
2023-05-19 15:30:47 +0800

    added unit testing with 'errorHandler.test.js'

---
+++ __tests__/middlewares/errorHandler.test.js
@@ -0,0 +1,17 @@
+const errorHandler = require('./../../middlewares/errorHandler');
+describe('MIDDLEWARE errorHandler', () => {
+    test('error', () => {
+        const err = {};
+        const req = {};
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+        const next = {};
+        errorHandler(err, req, res, next);
+        expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.json).toHaveBeenCalledWith({
+            message: 'error'
+        });
+    });
+});
