7c6b2b26a0cbc375b8aa04cb6fd33e78906a74b1
Lorenz <lorenzgillesania20@gmail.com>
2023-05-31 17:18:22 +0800

    modified 'server.js' for CORS

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 require('express-async-errors');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3500;
 const connectToDB = require('./functions/connectToDB');
@@ -9,6 +10,13 @@ const errorHandler = require('./middlewares/errorHandler');
 const userRouter = require('./routes/userRouter');
 const taskRouter = require('./routes/taskRouter');
 const authRouter = require('./routes/authRouter');
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200,
+    methods: ['GET', 'POST', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 app.use(cookieParser());
