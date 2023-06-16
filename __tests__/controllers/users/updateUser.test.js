a54f0e1adec6ec85d7afd2773e5cd3f594e2985c
Lorenz <lorenzgillesania20@gmail.com>
2023-06-16 20:30:36 +0800

    modified 'updateUser.js' and 'updateUser.test.js' TO FIX THE UPDATE BUG IN USERS

--- __tests__/controllers/users/updateUser.test.js
+++ __tests__/controllers/users/updateUser.test.js
@@ -1,5 +1,5 @@
 const updateUser = require('./../../../controllers/users/updateUser');
-const { userFindOne, userUpdateOne } = require('./../../../models/users/userQueries');
+const { userFindOne, userUpdateOne, userFind } = require('./../../../models/users/userQueries');
 const bcrypt = require('bcrypt');
 const { validateId, validateUsername, validatePassword } = require('./../../../functions/validation');
 jest.mock('./../../../models/users/userQueries');
@@ -31,6 +31,56 @@ describe('PATCH api/users/:userId', () => {
         });
         expect(validateId).toHaveBeenCalledWith('id');
     });
+    test('invalid username', async () => {
+        const req = {
+            params: {
+                id: 'id'
+            },
+            body: {
+                username: 'username',
+                password: 'password'
+            }
+        };
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+        validateId.mockReturnValue(true);
+        validateUsername.mockReturnValue(false)
+        await updateUser(req, res);
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({
+            message: 'invalid username. underscores, letters and numbers only'
+        });
+        expect(validateId).toHaveBeenCalledWith('id');
+        expect(validateUsername).toHaveBeenCalledWith('username');
+    });
+    test('invalid password', async () => {
+        const req = {
+            params: {
+                id: 'id'
+            },
+            body: {
+                username: 'username',
+                password: 'password'
+            }
+        };
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        }
+        validateId.mockReturnValue(true);
+        validateUsername.mockReturnValue(true);
+        validatePassword.mockReturnValue(false);
+        await updateUser(req, res);
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({
+            message: 'invalid password. minimum 8 characters with atleast one letter, one number and one special character'
+        });
+        expect(validateId).toHaveBeenCalledWith('id');
+        expect(validateUsername).toHaveBeenCalledWith('username');
+        expect(validatePassword).toHaveBeenCalledWith('password');
+    });
     test('user not found', async () => {
         const STATIC_PROJECTION = '_id username password';
         const req = {
@@ -47,6 +97,8 @@ describe('PATCH api/users/:userId', () => {
             json: jest.fn()
         }
         validateId.mockReturnValue(true);
+        validateUsername.mockReturnValue(true);
+        validatePassword.mockReturnValue(true);
         userFindOne.mockResolvedValue(null);
         await updateUser(req, res);
         expect(res.status).toHaveBeenCalledWith(404);
@@ -56,8 +108,9 @@ describe('PATCH api/users/:userId', () => {
         expect(validateId).toHaveBeenCalledWith('id');
         expect(userFindOne).toHaveBeenCalledWith({_id: 'id'}, STATIC_PROJECTION);
     });
-    test('user updated', async () => {
-        const STATIC_PROJECTION = '_id username password';
+    test('user already taken', async () => {
+        const STATIC_PROJECTION_1 = '_id username password';
+        const STATIC_PROJECTION_2 = '_id';
         const STATIC_SALTROUNDS = 10;
         const req = {
             params: {
@@ -80,12 +133,52 @@ describe('PATCH api/users/:userId', () => {
         });
         validateUsername.mockReturnValue(true);
         validatePassword.mockReturnValue(true);
+        userFind.mockResolvedValue([
+            { _id: '_id_3'}
+        ]);
+        bcrypt.hashSync.mockReturnValue('hashedPassword');
+        await updateUser(req, res);
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({message: 'username already taken'});
+        expect(validateId).toHaveBeenCalledWith('_id_1');
+        expect(userFindOne).toHaveBeenCalledWith({_id: '_id_1'}, STATIC_PROJECTION_1);
+        expect(validateUsername).toHaveBeenCalledWith('username_1');
+        expect(validatePassword).toHaveBeenCalledWith('password_1');
+        expect(userFind).toHaveBeenCalledWith({username: 'username_1'}, STATIC_PROJECTION_2);
+    });
+    test('user updated', async () => {
+        const STATIC_PROJECTION_1 = '_id username password';
+        const STATIC_PROJECTION_2 = '_id';
+        const STATIC_SALTROUNDS = 10;
+        const req = {
+            params: {
+                id: '_id_1'
+            },
+            body: {
+                username: 'username_1',
+                password: 'password_1'
+            }
+        };
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+        validateId.mockReturnValue(true);
+        userFindOne.mockResolvedValue({
+            _id: '_id_2',
+            username: 'username_2',
+            password: 'password_2'
+        });
+        validateUsername.mockReturnValue(true);
+        validatePassword.mockReturnValue(true);
+        userFind.mockResolvedValue([]);
         bcrypt.hashSync.mockReturnValue('hashedPassword');
         await updateUser(req, res);
         expect(res.status).toHaveBeenCalledWith(200);
         expect(res.json).toHaveBeenCalledWith({message: 'user updated'});
         expect(validateId).toHaveBeenCalledWith('_id_1');
-        expect(userFindOne).toHaveBeenCalledWith({_id: '_id_1'}, STATIC_PROJECTION);
+        expect(userFindOne).toHaveBeenCalledWith({_id: '_id_1'}, STATIC_PROJECTION_1);
+        expect(userFind).toHaveBeenCalledWith({username: 'username_1'}, STATIC_PROJECTION_2);
         expect(bcrypt.hashSync).toHaveBeenCalledWith('password_1', STATIC_SALTROUNDS);
         expect(userUpdateOne).toHaveBeenCalledWith(
             {
--- controllers/users/updateUser.js
+++ controllers/users/updateUser.js
@@ -1,4 +1,4 @@
-const { userFindOne, userUpdateOne } = require('./../../models/users/userQueries');
+const { userFind, userFindOne, userUpdateOne } = require('./../../models/users/userQueries');
 const bcrypt = require('bcrypt');
 const { validateId, validateUsername, validatePassword } = require('./../../functions/validation');
 const updateUser = async (req, res) => {
@@ -10,6 +10,7 @@ const updateUser = async (req, res) => {
     const validatedUsername = validateUsername(username);
     const validatedPassword = validatePassword(password);
     let user = null;
+    let findUsersWithSimilarUsername = null;
     let hashedPassword = '';
     let update = {};
     let response = null;
@@ -18,6 +19,16 @@ const updateUser = async (req, res) => {
             status: 400,
             message: 'invalid id'
         };
+    }else if(typeof username !== 'undefined' && validatedUsername === false){
+        response = {
+            status: 400,
+            message: 'invalid username. underscores, letters and numbers only'
+        }
+    }else if(typeof password !== 'undefined' && validatedPassword === false){
+        response = {
+            status: 400,
+            message: 'invalid password. minimum 8 characters with atleast one letter, one number and one special character'
+        }
     }else{
         user = await userFindOne({_id: id}, '_id username password');
         if(user === null){
@@ -26,17 +37,30 @@ const updateUser = async (req, res) => {
                 message: 'user not found'
             };
         }else{
-            if(typeof username === 'string' && validatedUsername === true){
-                update.username = username;
+            if(typeof username !== undefined){
+                findUsersWithSimilarUsername = await userFind(
+                    {username: username}, '_id');
             }
-            if(typeof password === 'string' && validatedPassword === true){
-                hashedPassword = bcrypt.hashSync(password, saltRounds)
-                update.password = hashedPassword;
-            }
-            await userUpdateOne({_id: user._id.toString()}, update);
-            response = {
-                status: 200,
-                message: 'user updated'
+            if(typeof username !== undefined
+                && findUsersWithSimilarUsername.length !== 0
+            ){
+                response = {
+                    status: 400,
+                    message: 'username already taken'
+                }
+            }else{
+                if(typeof username === 'string' && validatedUsername === true){
+                    update.username = username;
+                }
+                if(typeof password === 'string' && validatedPassword === true){
+                    hashedPassword = bcrypt.hashSync(password, saltRounds)
+                    update.password = hashedPassword;
+                }
+                await userUpdateOne({_id: user._id.toString()}, update);
+                response = {
+                    status: 200,
+                    message: 'user updated'
+                }
             }
         }
     }
