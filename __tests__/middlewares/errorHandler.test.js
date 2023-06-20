70e0c47ee3bda07de39e3a4e0ff51b834ffdf250
Lorenz <lorenzgillesania20@gmail.com>
2023-06-20 13:43:22 +0800

    modified 'errorHandler.js' and 'errorHandler.test.js' TO GIVE APPROPRIATE MESSAGE WHEN JWT TOKEN EXPIRED

--- __tests__/middlewares/errorHandler.test.js
+++ __tests__/middlewares/errorHandler.test.js
@@ -1,5 +1,22 @@
 const errorHandler = require('./../../middlewares/errorHandler');
 describe('MIDDLEWARE errorHandler', () => {
+    test('jwt expired', () => {
+        const err = {
+            stack: 'errorStack',
+            message: 'jwt expired'
+        };
+        const req = {};
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+        const next = {};
+        errorHandler(err, req, res, next);
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({
+            message: 'jwt expired'
+        });
+    });
     test('error', () => {
         const err = {
             stack: 'errorStack'
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,7 +1,19 @@
 const errorHandler = (err, req, res, next) => {
+    let response = null;
     if(process.env.NODE_ENV !== 'production'){
         console.log(err.stack);
     }
-    return res.status(500).json({message: 'error'});
+    if(typeof err.message !== 'undefined' && err.message === 'jwt expired'){
+        response = {
+            status: 400,
+            message: err.message
+        }
+    }else{
+        response = {
+            status: 500,
+            message: 'error'
+        }
+    }
+    return res.status(response.status).json({message: response.message});
 }
 module.exports = errorHandler;
