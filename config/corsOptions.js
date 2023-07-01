5397e9206d8fb51de934a56899acef1289c91cb3
Lorenz <lorenzgillesania20@gmail.com>
2023-07-01 13:53:37 +0800

    modified 'corsOptions.js' READY TO DEPLOY BACKEND VERSION 3

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,8 +2,8 @@ const allowedOrigins = require('./allowedOriginsArray');
 const corsOptions = {
     origin: allowedOrigins,
     optionsSuccessStatus: 200,
-    methods: ['GET', 'POST', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    //methods: ['GET', 'POST', 'DELETE', 'PATCH'],
+    //allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true //to manage cookies using fetch()
 }
 module.exports = corsOptions;
