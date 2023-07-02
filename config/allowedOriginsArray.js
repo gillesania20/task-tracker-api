363e536294d3f6a095a0bf12194bb93d8dc6625e
Lorenz <lorenzgillesania20@gmail.com>
2023-07-02 14:49:42 +0800

    modified 'allowedOriginsArray.js'  READY TO DEPLOY BACKEND VERSION 8

--- config/allowedOriginsArray.js
+++ config/allowedOriginsArray.js
@@ -3,6 +3,6 @@ const allowedOriginsArray = [
     //'https://task-tracker-api-1xc2.onrender.com/',
     //'https://task-tracker-9mss.onrender.com'
     //'https://task-tracker-api-1xc2.onrender.com'
-    'https://task-tracker-production.up.railway.app/'
+    'https://task-tracker-production.up.railway.app'
 ];
 module.exports = allowedOriginsArray;
